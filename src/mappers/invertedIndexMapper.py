13eb8e653d5a515fbb8364eb45519f4ce168c83c
Priyansh <priyanshjain@Priyansh.local>
2018-03-15 23:47:06 -0700

    Initial commit

---
+++ src/mappers/invertedIndexMapper.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # split the line into words
+    words = line.split()
+
+    documentId = words.pop(0)
+    lineNumber = words.pop(0)
+
+    for index in range( len(words) ):
+        word = words[ index ]
+        print '{0}, {1}, {2}, {3}'.format( word, documentId, lineNumber, index )
+    
